876568264a8cd529ae53aa5d40f3761de7d45344
badexa <baderbelhajallal@example.com>
2024-05-20 21:38:11 +0100

    first

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const connectDB = require('./config/db')
-
+const router = require('./routes')
 
 
 const app = express()
@@ -12,6 +12,7 @@ app.use(cors({
 app.use(express.json())
 
 
+app.use("/api",router)
 
 
 const PORT = 8080 || process.env.PORT
